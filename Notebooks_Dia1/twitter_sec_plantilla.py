351b33840d904ab219d87e1a5bc80b04e23fe64b
Enrique Crespo <jecrespom@gmail.com>
2020-05-26 13:34:05 +0200

    Dia1

---
+++ Notebooks_Dia1/twitter_sec_plantilla.py
@@ -0,0 +1,8 @@
+# Reemplaza las xxxx por el código correspondiente...
+# 
+
+CONSUMER_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxx"
+CONSUMER_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+ACCESS_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ACCESS_TOKEN_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
